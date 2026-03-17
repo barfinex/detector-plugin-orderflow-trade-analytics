35de39e092bd63590e5cbfe42e53fffd7a3531fe
barfinex-ci <ci@barfinex.com>
2026-03-17 20:04:12 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './orderflow-trade-analytics.module';
 export * from './orderflow-trade-analytics.controller';
 export * from './orderflow-trade-analytics.service';
+export * from './orderflow-trade-analytics.constants';
 export * from './types/metrics.types';
 
